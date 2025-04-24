e31a73ff0e85ea21a375069360bbaaf297bed271
fullmetalashley <73601224+fullmetalashley@users.noreply.github.com>
2025-04-23 18:44:57 -0700

    Base YarnSpinner functionality set up.
    YarnSpinner is working with interactables, in a test scene.

---
+++ Nemefish/Assets/Scripts/Interactable.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+using Yarn.Unity;
+
+public class Interactable : MonoBehaviour
+{
+    public bool playerWithinRange;
+    public bool dialogueRunning;
+
+    private DialogueRunner _dialogueRunner;
+
+    public string yarnNode;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _dialogueRunner = FindAnyObjectByType<DialogueRunner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If we press the key and haven't already triggered dialogue...
+        if (Input.GetKeyDown(KeyCode.E) && !dialogueRunning)
+        {
+            //Trigger the dialogue.
+            dialogueRunning = true;
+            CallYarn();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //This is the player character, and this object is now interactable. 
+            this.playerWithinRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //This is the player character leaving the space, and the object is now un-interactable.
+            this.playerWithinRange = false;
+        }
+    }
+    public void CallYarn()
+    {
+        _dialogueRunner.StartDialogue(yarnNode);
+    }
+}
