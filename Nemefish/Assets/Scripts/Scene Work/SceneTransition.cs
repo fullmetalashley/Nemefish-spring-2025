797fc267b5141792d182d42fa523fefca241731e
fullmetalashley <73601224+fullmetalashley@users.noreply.github.com>
2025-05-22 20:18:58 -0700

    Updated scene transition to better work with player controller.
    Also updated player and FOHI prefabs. Removed FOHI tag, turned off raycast point on the player.

--- Nemefish/Assets/Scripts/Scene Work/SceneTransition.cs
+++ Nemefish/Assets/Scripts/Scene Work/SceneTransition.cs
@@ -5,6 +5,14 @@ public class SceneTransition : MonoBehaviour
 {
     public string sceneToLoad;
 
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            LoadNext();
+        }
+    }
     public void LoadNext()
     {
         System.Console.WriteLine("Transitioning to scene " + sceneToLoad);
