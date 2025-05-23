e127a900bb00edd0adc066e55ecb7969fe591767
Czar_Yoshi <9873796+NathanielMaciejewski@users.noreply.github.com>
2025-05-22 19:31:23 -0700

    Convert overworld to dirt blocks
    Transitions our three main overworld areas (southeast coast, east coast, north coast) to dirt block + water blocks instead of grayboxes. Story areas (point no point, player camp, old exterminator camp, abandoned town) aren't done yet. Moved all removed scenes to their own folder to reduce clutter.

--- Nemefish/Assets/Scripts/Scene Work/SceneTransition.cs
+++ Nemefish/Assets/Scripts/Scene Work/SceneTransition.cs
@@ -7,6 +7,7 @@ public class SceneTransition : MonoBehaviour
 
     public void LoadNext()
     {
+        System.Console.WriteLine("Transitioning to scene " + sceneToLoad);
         SceneManager.LoadScene(sceneToLoad);
     }
 }
