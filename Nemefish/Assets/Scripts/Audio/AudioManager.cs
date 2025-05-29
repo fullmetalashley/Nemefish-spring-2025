4779b3bca36f13f982b94d11708c9d150a95fc00
BB-Driver00 <168240866+BB-Driver00@users.noreply.github.com>
2025-05-29 09:02:41 -0700

    Fixed PTO Walla bug
    - fixed bug where ambience would initialize correctly but not stop

--- Nemefish/Assets/Scripts/Audio/AudioManager.cs
+++ Nemefish/Assets/Scripts/Audio/AudioManager.cs
@@ -5,6 +5,7 @@ using System.Dynamic;
 using System.Collections.Generic;
 using UnityEditor.SearchService;
 using UnityEngine.SceneManagement;
+using JetBrains.Annotations;
 
 public class AudioManager : MonoBehaviour
 {
@@ -15,6 +16,7 @@ public class AudioManager : MonoBehaviour
     private EventInstance musicEventInstance;
 
     public static AudioManager instance { get; private set; }
+    public EventInstance ptoWalla;
 
     private void Awake()
     {
@@ -28,20 +30,37 @@ public class AudioManager : MonoBehaviour
             // Debug.LogError("Found more than one Audio Manager in the scene.");
             Destroy(gameObject);
         }
-        
+
 
     }
 
     private void Start()
     {
-        var activeScene = SceneManager.GetActiveScene();
         InitializeScore(FMODEvents.instance.score);
         InitializeAmbience(FMODEvents.instance.coastAmbience);
         InitializeAmbience(FMODEvents.instance.forestAmbience);
+        ptoWalla = instance.CreateEventInstance(FMODEvents.instance.ptoWalla);
+       
+    }
+
+    void Update()
+    {
+        var activeScene = SceneManager.GetActiveScene();
+
         if (activeScene.name == "Player Camp")
         {
-            InitializeAmbience(FMODEvents.instance.ptoWalla);
+                        PLAYBACK_STATE playbackState;
+            ptoWalla.getPlaybackState(out playbackState);
+            if (playbackState.Equals(PLAYBACK_STATE.STOPPED))
+            {
+                ptoWalla.start();
+            }
         }
+        else
+        {
+            ptoWalla.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        }
+        
     }
 
     private void InitializeAmbience(EventReference ambienceEventReference)
--- Nemefish/Assets/Scripts/Scene Work/SceneTransition.cs
+++ Nemefish/Assets/Scripts/Scene Work/SceneTransition.cs
@@ -15,35 +15,13 @@ public class SceneTransition : MonoBehaviour
     public float fohiSpawnY;
     public float fohiSpawnZ;
 
-    private EventInstance ptoWalla;
-    // private AudioManager audioManager;
-
-    public void Start()
-    {
-        // audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
-        ptoWalla = AudioManager.instance.CreateEventInstance(FMODEvents.instance.ptoWalla);
-    }
-
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             LoadNext();
         }
-        if (sceneToLoad == "Player Camp")
-        {
-            // audioManager.InitializeAmbience(FMODEvents.instance.ptoWalla);
-            PLAYBACK_STATE playbackState;
-            ptoWalla.getPlaybackState(out playbackState);
-            if (playbackState.Equals(PLAYBACK_STATE.STOPPED))
-            {
-                ptoWalla.start();
-            }
-        }
-        else
-        {
-            ptoWalla.stop(STOP_MODE.ALLOWFADEOUT);
-        }
+
     }
 
     public void LoadNext()
