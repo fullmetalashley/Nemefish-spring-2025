e2cb29cd65540b28a2028e820b3d0fcd3ed72d05
BB-Driver00 <168240866+BB-Driver00@users.noreply.github.com>
2025-06-04 16:56:57 -0700

    Began to add ambience change parameters
    - added ambience change triggers to MapDev scene

--- Nemefish/Assets/Scripts/Audio/AmbienceChangeTrigger.cs
+++ Nemefish/Assets/Scripts/Audio/AmbienceChangeTrigger.cs
@@ -6,11 +6,12 @@ public class AmbienceChangeTrigger : MonoBehaviour
     [SerializeField] private string parameterName;
     [SerializeField] private float parameterValue;
 
-    private void OgerEnter2D(Collider2D collider)
+    private void OnTriggerEnter(Collider collider)
     {
         if (collider.tag.Equals("Player"))
         {
             AudioManager.instance.SetAmbienceParameter(parameterName, parameterValue);
+            Debug.Log(parameterName + " value " + parameterValue);
         }
     }
 }
